3f8f0025afb9f066dab6b1dfaa948e3f97247e93
elliehachemjob <elliehachemjob@gmail.com>
2021-10-29 16:20:03 +0300

    importing and preprating to use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { ClientWidget } from "./ClientWidget";
+import { AdminPanel } from "./AdminPanel";
+import { AdminPanelFilled } from "./AdminPanelFilled";
+import { ExpansionAdminPanel } from "./ExpansionAdminPanel";
+import { LoginForm, SignUpForm } from "./Forms";
+import { SidebarExpansion } from "./SidebarExpansion";
+import { VideoCall } from "./VideoCall";
+import { VoiceCall } from "./VoiceCall";
 
 function App() {
-  return <div className="App"></div>;
+  const [isWidget, setIsWidget] = useState(false);
+
+  return (
+    <div className="App">
+      <ClientWidget isWidget={isWidget} setIsWidget={setIsWidget} />
+    </div>
+  );
 }
 
 export default App;
