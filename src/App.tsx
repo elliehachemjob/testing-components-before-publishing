4f3f47d22fad030c0fff32249416e234f3b15694
elliehachemjob <elliehachemjob@gmail.com>
2021-10-29 16:37:54 +0300

    still finishing the client component

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,21 @@ import { VideoCall } from "./VideoCall";
 import { VoiceCall } from "./VoiceCall";
 
 function App() {
-  const [isWidget, setIsWidget] = useState(false);
+  const [isWidget, setIsWidget] = useState<boolean>(false);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const [user, setUser] = useState<string>("");
+
+  const sendMessage = async (user: string, isAdmin: string) => {};
 
   return (
     <div className="App">
-      <ClientWidget isWidget={isWidget} setIsWidget={setIsWidget} />
+      <ClientWidget
+        isWidget={isWidget}
+        setIsWidget={setIsWidget}
+        sendMessage={sendMessage}
+        normalImage="https://media-exp1.licdn.com/dms/image/C4E0BAQG9rQefXRgLBQ/company-logo_200_200/0/1625823645566?e=2159024400&v=beta&t=x-KSyk7s-GdJmqjj6Y-KKYDzr71xAg7wgE9oZBGMd4E"
+        flippedImage="https://i.ibb.co/xXsFSqM/1625823645566-e-2159024400-v-beta-t-x-KSyk7s-Gd-Jmqjj6-Y-KKYDzr71x-Ag7wg-E9o-ZBGMd4-E.jpg"
+      />
     </div>
   );
 }
--- src/ClientWidget/ClientWidget.tsx
+++ src/ClientWidget/ClientWidget.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Widget } from 'react-chat-widget';
+import React from "react";
+import "./App.css";
+import { Widget } from "react-chat-widget";
 // @ts-ignore
-import flippedimage from './normalImage.png';
+import flippedImage from "./normalImage.png";
 // @ts-ignore
-import normalimage from './flippedImage.png';
+import normalImage from "./flippedImage.png";
 
 interface Props {
   isWidget: boolean;
   setIsWidget: (active: boolean) => void;
-  sendMessage?: (user: string, isAdmin: string) => Promise<void>;
-  normalimage?: string;
+  sendMessage: (user: string, isAdmin: string) => Promise<void>;
+  normalImage?: string;
   flippedImage?: string;
   normalImageAlt?: string;
   flippedimageAlt?: string;
@@ -25,10 +25,10 @@ export const ClientWidget: React.FC<Props> = (props: any): any => {
     if (props.isWidget) {
       return (
         <img
-          src={props.normalimage ? props.normalimage : normalimage}
-          alt={props.normalimageAlt ? props.normalimageAlt : 'imageNot working'}
+          src={props.normalImage ? props.normalImage : normalImage}
+          alt={props.normalimageAlt ? props.normalimageAlt : "imageNot working"}
           className={
-            props.normalImageStyle ? props.normalImageStyle : 'rcw-launcher'
+            props.normalImageStyle ? props.normalImageStyle : "rcw-launcher"
           }
           onClick={() => {
             handleToggle();
@@ -39,12 +39,12 @@ export const ClientWidget: React.FC<Props> = (props: any): any => {
     } else {
       return (
         <img
-          src={props.flippedimage ? props.flippedimage : flippedimage}
+          src={props.flippedImage ? props.flippedImage : flippedImage}
           alt={
-            props.flippedimageAlt ? props.flippedimageAlt : 'imageNot working'
+            props.flippedimageAlt ? props.flippedimageAlt : "imageNot working"
           }
           className={
-            props.flippedImageStyle ? props.flippedImageStyle : 'rcw-launcher'
+            props.flippedImageStyle ? props.flippedImageStyle : "rcw-launcher"
           }
           onClick={() => {
             handleToggle();
@@ -62,9 +62,9 @@ export const ClientWidget: React.FC<Props> = (props: any): any => {
           props.sendMessage(props.user, props.isAdmin, e)
         }
         launcher={(handleToggle: any) => getCustomLauncher(handleToggle)}
-        title={props.title ? props.title : 'Plugit Chat Support'}
+        title={props.title ? props.title : "Plugit Chat Support"}
         subtitle={
-          props.subtitle ? props.subtitle : 'Welcome To Yoonit Customer Service'
+          props.subtitle ? props.subtitle : "Welcome To Yoonit Customer Service"
         }
         senderPlaceHolder="press send button or enter to send a message"
       />
